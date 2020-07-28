8bbd48933b5cc7b77dc197cf64ba814d71e4bf50
刘欢 <184555556@qq.com>
2020-07-29 00:30:05 +0800

    首页基本完成，增加轮播图、首页显示内容

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="goods-list">
+    <goods-list-item class="item" v-for="item in goods" :goods-item="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem";
+
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem,
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  created() {
+    // console.log(this.goods);
+  },
+};
+</script>
+
+<style>
+.goods-list {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px;
+  justify-content: space-around;
+  /* justify-content: space-evenly; */
+}
+
+.goods-list .item {
+  width: 48%;
+}
+</style>
